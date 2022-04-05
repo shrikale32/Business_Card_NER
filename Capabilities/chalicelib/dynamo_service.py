487b6fdf046db872a8e89bce6d9bc2dd133afe54
Néstor Romero León <romero.nestor@gmail.com>
2022-04-05 09:06:12 -0400

    Search service update for scanning items

--- Capabilities/chalicelib/dynamo_service.py
+++ Capabilities/chalicelib/dynamo_service.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 import boto3
-import uuid, json
+import uuid
+import json
 
 from business_card import BusinessCard
 
@@ -11,7 +12,7 @@ class DynamoService:
         self.dynamodb = boto3.client('dynamodb')
 
     def store_card(self, card: BusinessCard):
-        
+
         # Ensure primary key - low collision
         card.card_id = str(uuid.uuid4())
         response = self.dynamodb.put_item(
@@ -32,50 +33,50 @@ class DynamoService:
     def delete_card(self, card_id):
         response = self.dynamodb.delete_item(
             TableName=self.table_name,
-            Key={'card_id': {'S' : str(card_id)}}
+            Key={'card_id': {'S': str(card_id)}}
         )
         return response
 
     def get_card(self, card_id):
         response = self.dynamodb.get_item(
             TableName=self.table_name,
-            Key={'card_id': {'S' : card_id}}
+            Key={'card_id': {'S': card_id}}
         )
         return response
 
     def search_cards(self, filter=None, sort=None):
-        response = self.dynamodb.query(
+        response = self.dynamodb.scan(
             TableName=self.table_name,
-            Select='ALL_ATTRIBUTES',
-            Limit = 10,
-            KeyConditionExpression= 'card_names = :filter_criteria',
-            ExpressionAttributeValues={
-                ':filter_criteria': {'S': filter}
-            
-        }
-)
+            Limit=10,
+            ProjectionExpression= "card_id, card_names, email_addresses, company_name",
+            # FilterExpression='card_names = :filter_criteria',
+            # ExpressionAttributeValues={
+            #     ':filter_criteria': {'S': filter}
+            # }
+        )
+        return response
 
 
 if __name__ == '__main__':
-    
+
     dynamo = DynamoService('BusinessCards')
-    
-    for idx in range(1,20,1):
-        card = BusinessCard(12345, f'Nero{idx}', [55567890], [
-                        'pepe@pepe.com'], f'NeroCorp{idx}', 'www.nero.com.co', f'123-{idx} address road')
-        response = dynamo.store_card(card)
-        print('Card Created')
-        pprint(response)    
-    
+
+    # for idx in range(1,51,1):
+    #     card = BusinessCard(12345, f'Nero{idx}', [55567890], [
+    #                     'pepe@pepe.com'], f'NeroCorp{idx}', 'www.nero.com.co', f'123-{idx} address road')
+    #     response = dynamo.store_card(card)
+    #     print('Card Created')
+    #     pprint(response)
+
     response = dynamo.search_cards('Nero1')
     print('Search filter')
     print(response)
 
     # response = dynamo.get_card('')
-    
+
     # print('Card 2 Retrieved')
     # pprint(response)
-    
+
     # card2 = BusinessCard(12346, 'Nero2', [555678902], [
     #                      'pepe2@pepe.com'], 'NeroCorp2', 'www.nero2.com.co', '234 address road')
     # dynamo = DynamoService('BusinessCards')
